8786576af709001523e6d6450bcb51944dd3ef16
Your Name <you@example.com>
2022-06-08 13:21:09 -0300

    Aula08

---
+++ pcweb/Aula08/main.js
@@ -0,0 +1,8 @@
+document.getElementById("submiting").addEventListener("click",()=>
+{
+    const currentDate = new Date()
+    const birthDate = document.getElementById("date")
+    let allTime= currentDate.getHours
+    console.log(allTime)
+})
+
